62ea00d675cc9614128e997b1eabb50800f0754b
mohibraja11 <mohibraja11@gmail.ccom>
2022-10-15 03:00:21 +0100

    implementing modularization

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.spring.io/release")
+    }
+    maven {
+        url = uri("https://repository.jboss.org/maven2")
+    }
+}
