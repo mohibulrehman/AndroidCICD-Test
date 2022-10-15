2b012c9d01206f9510fe867037bc464a2472bca8
mohibraja11 <mohibraja11@gmail.ccom>
2022-10-15 03:09:56 +0100

    first step for modulatization

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,11 +3,7 @@ plugins {
 }
 
 repositories {
+
     mavenCentral()
-    maven {
-        url = uri("https://repo.spring.io/release")
-    }
-    maven {
-        url = uri("https://repository.jboss.org/maven2")
-    }
+    
 }
