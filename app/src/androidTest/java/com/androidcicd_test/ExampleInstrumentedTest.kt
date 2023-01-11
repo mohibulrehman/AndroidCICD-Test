467a0bc0887e879a2864fa7a051ebd7be6e5e7af
Mohib Rehman <mohib.rehman@proact.net>
2023-01-11 15:37:34 +0000

    working fine

--- app/src/androidTest/java/com/androidcicd_test/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/androidcicd_test/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.androidcicd_test1", appContext.packageName)
+        assertEquals("com.androidcicd_test", appContext.packageName)
     }
 }
